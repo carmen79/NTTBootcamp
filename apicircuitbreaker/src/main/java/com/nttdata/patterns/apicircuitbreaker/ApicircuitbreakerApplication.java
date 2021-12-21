6097b58b07d138f86ae4e72bf0dfbee3c71073fe
Carmen <carmen.buevig@gmail.com>
2021-12-21 18:06:30 +0100

    apicircuitBreaker

---
+++ apicircuitbreaker/src/main/java/com/nttdata/patterns/apicircuitbreaker/ApicircuitbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.patterns.apicircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicircuitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicircuitbreakerApplication.class, args);
+	}
+
+}
---
+++ apicircuitbreaker/src/test/java/com/nttdata/patterns/apicircuitbreaker/ApicircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.patterns.apicircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
